9e5b4935606c2b300edd9b26f5a80a4121eb38ae
Frederic Soc <tocomorales30@gmail.com>
2024-11-21 16:24:07 -0600

    Inicializando el proyecto

---
+++ src/DB_Coneccion.js
@@ -0,0 +1,31 @@
+import sql from 'mssql'
+import dotenv from 'dotenv'
+import process from 'node:process'
+
+dotenv.config()
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.PASSWORD,
+  server: process.env.SERVER,
+  database: process.env.DATABASE,
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
+}
+
+export async function InsertUser(userIP) {
+  try {
+    const pool = await sql.connect(config)
+    const query = await pool
+      .request()
+      .input('UserIP', sql.VarChar, userIP)
+      .query('INSERT INTO PHISHING_TEST(UserIP) Values(@UserIP)')
+    console.log(query)
+  } catch (error) {
+    console.error(`Error en la consulta ${error}`)
+  } finally {
+    sql.close()
+  }
+}
---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import fs from 'node:fs'
+import { InsertUser } from './DB_Coneccion.js'
+
+const app = express()
+const clickLogFile = 'clicks.json'
+
+app.get('', (req, res) => {
+  const userId = req.query.id
+  const clickData = {
+    userId: userId,
+    timestamp: new Date().toISOString(),
+    ipAddress: req.ip
+  }
+
+  InsertUser(req.ip)
+
+  fs.readFile(clickLogFile, (err, data) => {
+    const clicks = data ? JSON.parse(data) : []
+    clicks.push(clickData)
+    fs.writeFile(clickLogFile, JSON.stringify(clicks), (err) => {
+      if (err) console.error('Error guardando clic:', err)
+    })
+  })
+
+  res.send(
+    '<h1>¡Has caído en una simulación de phishing!</h1><p>Aprende cómo evitar estos ataques...</p>'
+  )
+})
+
+app.listen(7000, () => {
+  console.log('Servidor escuchando en http://localhost:7000')
+})
